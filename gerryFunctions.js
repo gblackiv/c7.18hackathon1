9b070e15c6d437e94f1a717079931c4f6b90433c
Mia Tran <maiptran157@gmail.com>
2018-08-22 16:20:46 -0700

    fixed winCounter

--- gerryFunctions.js
+++ gerryFunctions.js
@@ -8,8 +8,10 @@ function switchPlayer(){
 }
 
 //returns the currentGameBoard, so when called, you must assign to the global variable gameBoard variable
-function startGame(){
-		winCounter = parseInt($('.selectWinningCounter option:selected').val());
+function startGame(notResetGame = true){
+	if(notResetGame){
+		winCounter = (parseInt($('.selectWinningCounter option:selected').val()))-1;
+	}
 		var cssSize = 100 / boardSize;
 		var cssTextSize = 55 / boardSize ;
 	for( let outter = 0; outter < boardSize; outter++ ){
