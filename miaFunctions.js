5a20f310c81d023fe1184b8abec6a543f46d2edd
Mia Tran <maiptran157@gmail.com>
2018-08-22 11:05:26 -0700

    edit boardSize and create winningOption HTML

--- miaFunctions.js
+++ miaFunctions.js
@@ -1,8 +1,2 @@
-var boardSize = 3; //hardcode for now but will take in user input later
-var currentGameBoard = createBoardSizeArray(boardSize);
-
-function createBoardSizeArray(boardSizeInput) {
-
-return new Array( new Array (boardSizeInput));
-
-}
+var boardSize = null; //hardcode for now but will take in user input later
+var currentGameBoard = [];
