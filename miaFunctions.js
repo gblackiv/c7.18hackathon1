a5e6da7ef580726fcf68781bd7aed3310f1d7834
Mia Tran <maiptran157@gmail.com>
2018-08-22 12:22:18 -0700

    create function to record first click

--- miaFunctions.js
+++ miaFunctions.js
@@ -1,2 +1,19 @@
+$(document).ready(initializeApp)
+function initializeApp(){
+    firstSubmitClickHandler();
+}
 var boardSize = null; //hardcode for now but will take in user input later
 var currentGameBoard = [];
+
+var firstClick = null;
+
+function firstSubmitClickHandler() {
+    $('.gameBoardContainer').on( 'click', '.submitButton', recordfFirstSubmitClick );
+}
+
+function recordfFirstSubmitClick() {
+boardSize = $('.selectBoardSize option:selected').val();
+createWinConditionMenu(boardSize);
+}
+
+
