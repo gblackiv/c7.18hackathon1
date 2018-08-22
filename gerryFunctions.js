5049c261fcfb7005b2c5ff224c94f987e1378ac6
Gerry Blackmon <blackmongerry@gmail.com>
2018-08-22 09:49:35 -0700

    startGame function added

---
+++ gerryFunctions.js
@@ -0,0 +1,11 @@
+//returns the currentGameBoard, so when called, you must assign to the global variable gameBoard variable
+function startGame(boardSizeP, currentGameBoardP){
+	for( let itterations = 0; itterations < boardSizeP; itterations++ ){
+		for( let inner = 0; inner < boardSizeP; inner++ ){
+			var newSquare = $( '<div>', {class: 'square'} );
+			$(`.row::nth-child(${outter}`).append(newSquare);
+			currentGameBoardP[outter][inner].push(newSquare);
+		}
+	}
+	return currentGameBoardP;
+}
