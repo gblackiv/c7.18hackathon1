39a662b6d4e7023297d7024a176bc8badd9206dd
Mia Tran <maiptran157@gmail.com>
2018-08-22 10:01:17 -0700

    create boardSize variable and currentGameBoard array

---
+++ miaFunctions.js
@@ -0,0 +1,8 @@
+var boardSize = 3; //hardcode for now but will take in user input later
+var currentGameBoard = createBoardSizeArray(boardSize);
+
+function createBoardSizeArray(boardSizeInput) {
+
+return new Array( new Array (boardSizeInput));
+
+}
